96857921e82b0a7a59b614ebe7bb0f7da8839ba8
Dmitrij Sokolov <DmitrijVS@gmail.com>
2022-06-21 08:51:57 +0300

    Добавьте файлы проекта.

---
+++ Modul8Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul8Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
