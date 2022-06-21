0b3fd6883ce158008c026f9f9f0792d377e91c00
Dmitrij Sokolov <DmitrijVS@gmail.com>
2022-06-21 16:26:35 +0300

    Перенос на другой комп

--- Modul8Task2/Program.cs
+++ Modul8Task2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
 
 namespace Modul8Task2
 {
@@ -6,7 +9,59 @@ namespace Modul8Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string folder = @"D:\Temp";
+            long folderSize = 0;
+
+            // Ввод и провека папки на существование
+            Console.WriteLine("Введите директорию для расчёта занимаемого места :");
+            folder = Console.ReadLine();
+
+            if (!Directory.Exists(folder))
+            {
+                Console.WriteLine(" Вы ввели не существующую директорию. Программа прекращает свою работу.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Начат расчёт размеров директории " + folder);
+
+            GetDirSize(folder, ref folderSize);
+
+            Console.WriteLine("Размер директории = " + folderSize);
+
+            Console.ReadKey();
+
+        }
+
+        public static void GetDirSize(string aFolder, ref long folderSize)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(aFolder);
+            DirectoryInfo[] arrDir = dirInfo.GetDirectories();
+            FileInfo[] arrFile = dirInfo.GetFiles();
+
+            //Console.WriteLine(dirInfo.FullName);      отладочная информация, затереть перед сдачей
+
+            try
+            {
+                // Суммируем размер файлов в директории
+                foreach (var file in arrFile)
+                {
+                    folderSize = (folderSize + file.Length);
+
+                    //Console.WriteLine(file.FullName);  отладочная информация, затереть перед сдачей
+                }
+
+                // Рекурсивно проходим по всем остальним директориям
+                foreach (var dir in arrDir)
+                {
+                    GetDirSize(dir.FullName, ref folderSize);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Не удалось считать размер файла или директории по причине\n " + ex.Message);
+            }
+
         }
     }
 }
